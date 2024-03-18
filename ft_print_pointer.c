f77c8819105cdb9ddd44f3163db20501e3f442f4
flomero <49640251+flomero@users.noreply.github.com>
2024-03-18 12:04:09 +0100

    fix pointer stuff

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,18 +6,19 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/09 12:57:02 by flfische          #+#    #+#             */
-/*   Updated: 2024/03/09 13:27:40 by flfische         ###   ########.fr       */
+/*   Updated: 2024/03/18 12:01:40 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-int	ft_numlen_base(unsigned long long n, int base)
+int	ft_numlen_base(unsigned long n, int base)
 {
 	int	len;
 
 	len = 1;
-	while (n >= (unsigned long long)base)
+	while (n >= (unsigned long)base)
 	{
 		n /= base;
 		len++;
@@ -35,32 +36,54 @@ int	ft_putnchr_fd(char c, int n, int fd)
 	return (size);
 }
 
+int	ft_print_pointer_hex(unsigned long pointer)
+{
+	int	size;
+
+	size = 0;
+	if (pointer >= 16)
+	{
+		size += ft_print_pointer_hex(pointer / 16);
+		size += ft_putchar_fd("0123456789abcdef"[pointer % 16], 1);
+	}
+	else
+		size += ft_putchar_fd("0123456789abcdef"[pointer], 1);
+	return (size);
+}
+
+int	ft_print_pointer_minus(t_format *format, unsigned long pointer)
+{
+	int	size;
+
+	size = 0;
+	ft_putstr_fd("0x", 1);
+	ft_putnchr_fd('0', format->precision - size + 2, 1);
+	if (!(format->precision == 0 && pointer == 0))
+		ft_print_pointer_hex(pointer);
+	ft_putnchr_fd(' ', format->width - size, 1);
+	return (size);
+}
+
 int	ft_print_pointer(t_format *format, va_list args)
 {
-	unsigned long long	pointer;
-	int					size;
+	unsigned long	pointer;
+	int				size;
 
-	pointer = va_arg(args, unsigned long long);
+	pointer = (unsigned long)va_arg(args, long);
 	size = ft_numlen_base(pointer, 16) + 2;
 	if (format->precision > size - 2)
 		size = format->precision + 2;
 	if (format->precision == 0 && pointer == 0)
 		size = 2;
 	if (format->flags.minus)
-	{
-		ft_putstr_fd("0x", 1);
-		ft_putnchr_fd('0', format->precision - size + 2, 1);
-		if (!(format->precision == 0 && pointer == 0))
-			ft_putnbr_base(pointer, "0123456789abcdef");
-		ft_putnchr_fd(' ', format->width - size, 1);
-	}
+		size += ft_print_pointer_minus(format, pointer);
 	else
 	{
 		ft_putnchr_fd(' ', format->width - size, 1);
 		ft_putstr_fd("0x", 1);
 		ft_putnchr_fd('0', format->precision - size + 2, 1);
 		if (!(format->precision == 0 && pointer == 0))
-			ft_putnbr_base(pointer, "0123456789abcdef");
+			ft_print_pointer_hex(pointer);
 	}
 	return (size);
 }

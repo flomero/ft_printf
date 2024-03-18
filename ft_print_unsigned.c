7c244cb1becf99929b5fd9d6b8806b0fdeb0ec23
flomero <49640251+flomero@users.noreply.github.com>
2024-03-18 14:10:20 +0100

    fix unsigned

--- ft_print_unsigned.c
+++ ft_print_unsigned.c
@@ -6,12 +6,40 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 09:30:10 by flfische          #+#    #+#             */
-/*   Updated: 2024/03/18 09:59:17 by flfische         ###   ########.fr       */
+/*   Updated: 2024/03/18 14:08:49 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
+
+int	ft_putunbr_fd(unsigned int n, int fd)
+{
+	int	size;
+
+	size = 0;
+	if (n >= 10)
+	{
+		size += ft_putunbr_fd(n / 10, fd);
+		size += ft_putchar_fd(n % 10 + '0', fd);
+	}
+	else
+		size += ft_putchar_fd(n + '0', fd);
+	return (size);
+}
+
+int	ft_get_uns_numlen(unsigned int n)
+{
+	int	size;
+
+	size = 1;
+	while (n >= 10)
+	{
+		n /= 10;
+		size++;
+	}
+	return (size);
+}
 
 int	ft_print_uns_minus(t_format *format_info, unsigned int num, int numlen)
 {
@@ -22,7 +50,7 @@ int	ft_print_uns_minus(t_format *format_info, unsigned int num, int numlen)
 		size += ft_putchar_fd('0', 1);
 	if (num == 0 && format_info->precision == -1)
 		return (size);
-	size += ft_putnbr_fd(num, 1);
+	size += ft_putunbr_fd(num, 1);
 	while (format_info->width-- > numlen)
 		size += ft_putchar_fd(' ', 1);
 	return (size);
@@ -34,8 +62,8 @@ int	ft_print_unsigned(t_format *format_info, va_list args)
 	int				numlen;
 	int				size;
 
-	num = va_arg(args, unsigned int);
-	numlen = ft_get_numlen(num);
+	num = (unsigned int)va_arg(args, unsigned int);
+	numlen = ft_get_uns_numlen(num);
 	size = 0;
 	if (format_info->flags.minus)
 		size += ft_print_uns_minus(format_info, num, numlen);
